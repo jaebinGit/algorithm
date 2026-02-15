4592383099de1fea277ebb3b3b0a9c1e2feb41f7
jaebinGit <132267743+jaebinGit@users.noreply.github.com>
2026-02-15 18:24:44 +0900

    Title: 사각형 채우기 3, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260215/사각형 채우기 3/rectangle-fill-3.java
+++ 260215/사각형 채우기 3/rectangle-fill-3.java
@@ -18,7 +18,7 @@ public class Main {
 
     private static int solve(int[] dp, int n) {
 
-        for(int i = 3; i <= n; i++) {
+        for(int i = 4; i <= n; i++) {
             dp[i] = 3*(dp[i-2]%1000000007) + 2*(dp[i-1]%1000000007) + 2*(dp[i-3]%1000000007) + 2*(dp[i-4]%1000000007);
         }
 
